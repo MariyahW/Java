641108f692e39a86c37908f4547e25129816cba1
MariyahW <149527258+MariyahW@users.noreply.github.com>
2024-12-12 19:05:11 -0500

    12/12/2024 @7 04pm

---
+++ hello/src/main/java/com/firstspring/hello/helloController.java
@@ -0,0 +1,7 @@
+package com.firstspring.hello;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class helloController {
+}
---
+++ runnerz/src/main/java/com/mariyahwatson/runnerz/RunnerzApplication.java
@@ -0,0 +1,20 @@
+package com.mariyahwatson.runnerz;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
+
+@SpringBootApplication
+public class RunnerzApplication {
+
+
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RunnerzApplication.class, args);
+		
+	}
+
+}
