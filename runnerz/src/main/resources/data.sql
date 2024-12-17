e75910887172fed61b8d45777b0346728bda1512
MariyahW <149527258+MariyahW@users.noreply.github.com>
2024-12-17 15:49:53 -0500

    added another test run

--- runnerz/src/main/resources/data.sql
+++ runnerz/src/main/resources/data.sql
@@ -1,2 +1,4 @@
 INSERT INTO Run (id, title, started_on, completed_on, miles, location)
 VALUES (1, 'My First Run', '2020-01-01 06:00:00', '2020-01-02 07:00:00', 10.0, 'OUTDOOR');
+INSERT INTO Run (id, title, started_on, completed_on, miles, location)
+VALUES (2, 'Another Run', '2024-01-01 06:00:00', '2024-01-02 07:00:00', 10.0, 'INDOOR');
