6431a3b7d669ad22b4175c148bb28acb2f59faec
MariyahW <149527258+MariyahW@users.noreply.github.com>
2024-12-17 15:12:15 -0500

    converted to temp sql daabase from in memory

--- runnerz/src/main/java/com/mariyahwatson/runnerz/run/RunRepo.java
+++ runnerz/src/main/java/com/mariyahwatson/runnerz/run/RunRepo.java
@@ -1,7 +1,9 @@
 package com.mariyahwatson.runnerz.run;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
@@ -11,37 +13,60 @@ import java.util.Optional;
 
 @Repository
 public class RunRepo {
-    private List<Run> runs= new ArrayList<>();
+    private final JdbcClient jdbcClient;
 
-    List<Run> findAll(){
-        return runs;
-    }
-    Optional<Run> findById(Integer id){
-        return runs.stream().filter(run -> run.id().equals(id)).findFirst();
+
+    public RunRepo(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
     }
 
-    //post
-    void create(Run run){
-        runs.add(run);
+    public List<Run> findAll() {
+        return jdbcClient.sql("select * from Run").query(Run.class).list();
+    }
+    public Optional<Run> findById(Integer id) {
+        return jdbcClient.sql("select id,title,started_on,completed_on,miles,location from Run where id=:id").param("id", id).query(Run.class).optional();
+    }
+    public void create(Run run) {
+        var updated=jdbcClient.sql("insert into Run(id, title, started_on, completed_on, miles, location) values (?,?,?,?,?,?)").params(List.of(run.id(), run.title(), run.startedOn(), run.completedOn(), run.miles(), run.location().toString())).update();
+        Assert.state(updated == 1, "Run not created."+ run.title());
+    }
+    public void update(Run run, Integer id) {
+        var updated=jdbcClient.sql("update Run set id=?, title=?, started_on=?, completed_on=?, miles=?, location=? where id=:run.id").params(List.of(run.id(), run.title(), run.startedOn(), run.completedOn(), run.miles(), run.location().toString())).update();
+        Assert.state(updated == 1, "Run not updated."+ run.title());
+    }
+    public void delete(Integer id) {
+        var updated = jdbcClient.sql("delete from run where id=:id").param("id", id).update();
+        Assert.state(updated == 1, "Run not deleted.");
+//    private List<Run> runs= new ArrayList<>();
+//    List<Run> findAll(){
+//        return runs;
+//    }
+//    Optional<Run> findById(Integer id){
+//        return runs.stream().filter(run -> run.id().equals(id)).findFirst();
+//    }
+//
+//    //post
+//    void create(Run run){
+//        runs.add(run);
+//    }
+//
+//    //delete
+//    void delete(Integer id){
+//        runs.removeIf(run -> run.id().equals(id));
+//    }
+//
+//    //put
+//    void update(Run run, Integer id){
+//        Optional<Run> optionalRun = findById(id);
+//        if(optionalRun.isPresent()){
+//            runs.set(runs.indexOf(optionalRun.get()), run);
+//        }
     }
 
-    //delete
-    void delete(Integer id){
-        runs.removeIf(run -> run.id().equals(id));
-    }
-
-    //put
-    void update(Run run, Integer id){
-        Optional<Run> optionalRun = findById(id);
-        if(optionalRun.isPresent()){
-            runs.set(runs.indexOf(optionalRun.get()), run);
-        }
-    }
-
-    @PostConstruct
-    private void init(){
-        runs.add(new Run(1, "Monday Run", LocalDateTime.now(), LocalDateTime.now().plus(30, ChronoUnit.MINUTES), 3, Location.INDOOR));
-        runs.add(new Run(2, "wednesday Run", LocalDateTime.now(), LocalDateTime.now().plus(60, ChronoUnit.MINUTES), 6, Location.OUTDOOR));
-    }
+//    @PostConstruct
+//    private void init(){
+//        runs.add(new Run(1, "Monday Run", LocalDateTime.now(), LocalDateTime.now().plus(30, ChronoUnit.MINUTES), 3, Location.INDOOR));
+//        runs.add(new Run(2, "wednesday Run", LocalDateTime.now(), LocalDateTime.now().plus(60, ChronoUnit.MINUTES), 6, Location.OUTDOOR));
+//    }
 
 }
---
+++ runnerz/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Run (id, title, started_on, completed_on, miles, location)
+VALUES (1, 'My First Run', '2020-01-01 06:00:00', '2020-01-02 07:00:00', 10.0, 'OUTDOOR');
